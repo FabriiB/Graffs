131cb25dc031881e686c55ceff9c842fc810c3d3
diegowrhasta <diegowrhasta@gmail.com>
2017-08-30 10:41:14 -0400

    Añadida la funcion en el objeto de Arista

--- app/src/main/java/com/example/fabricio/graffs/MainActivity.java
+++ app/src/main/java/com/example/fabricio/graffs/MainActivity.java
@@ -6,17 +6,16 @@ import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-
 import android.graphics.Path;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -452,6 +451,153 @@ public class MainActivity extends AppCompatActivity {
 
             canvas.drawPath(path, paint);
             canvas.restore();
+        }
+        // Funciones que asignaran a cada nodo su in, y su feed, al crear los nodos en su constructor su entrada y su feed
+        // deben ser de valor 0 para poder empezar a hacer los recorridos y poder comparar.
+        public void recorridoida()
+        {
+            try {
+                int s=0;
+                int s1=0;
+                int id2=(-1);
+                for(Arista x:edges)
+                {
+                    s=0;
+                    s1=0;
+                    for(Nodo a:nodes) {
+                        if (x.getX1() == a.getX() && x.getY1() == a.getY()) {
+                            s1=a.getStart();
+                            continue;
+                        } else if (x.getX2() == a.getX() && x.getY2() == a.getY()) {
+                            id2=a.getId();
+                            continue;
+                        }
+                    }
+                    s+=s1;
+                    s+=x.getPeso();
+                    if(nodes.get(id2).getStart()<s) //Reemplaza ahora con el mayor siempre en el Start del nodo siguiente, para que asi
+                                                    //siempre se quede en el start el mayor en caso de convergencia
+                    {
+                        nodes.get(id2).setStart(s);
+                    }
+                }
+               nodes.get(nodes.size()-1).setFeed(nodes.get(nodes.size()-1).getStart());
+            }
+            catch(Exception e)
+            {
+                Toast.makeText(MainActivity.this, "Surgió un error", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        public void recorridovuelta()
+        {
+            //ArrayLists que guardaran todos los nodos finales y de comienzo escaneados de la matriz
+            ArrayList<Integer> finales=new ArrayList<Integer>();
+            ArrayList<Integer> principios=new ArrayList<Integer>();
+            try {
+                int s=0;
+                int s1=0,s2=0;
+                int id2=(-1),id1=(-1);
+                double x1=0,x2=0,y1=0,y2=0;
+                int flag=0;
+                boolean papu=false;
+                for(int i=0;i<nodes.size();i++) //Comenzar a analizar la matriz para encontrar la fila donde tdo es 0, eso quiere decir que es un final de un camino
+                {
+                    flag = 0;
+                    for (int j = 0; j < nodes.size(); j++) {
+                        if (matrix[i][j] != 0) {
+                            flag = 1;
+                            break;
+                        }
+                    }
+                    if (flag == 0) //Si encuentra una fila con 0 encuentra un final y recupera ese nodo (El id para comparar)
+                    {
+                        Nodo aux = nodes.get(i);
+                        nodes.get(i).setFeed(nodes.get(i).getStart()); //Pone el Start Igual al Feed en este nodo extremo
+                        finales.add(aux.getId());
+                    }
+                }
+                int princol=0;
+                for(int i=0;i<nodes.size();i++) //Ahora analiza la matriz para encontrar principios, los principios son los que tienen columna donde tod es 0
+                {
+                    flag=0;
+                    if(matrix[i][princol]!=0);
+                        flag=1;
+                    if (flag == 0) //Si encuentra una columna con 0 encuentra un nodo de principio y recupera ese nodo (EL id para luego comparar)
+                    {
+                        Nodo aux = nodes.get(i);
+                        principios.add(aux.getId());
+                    }
+                    princol++;
+                }
+                for(int idfin:finales) { //Ahora recorrera a todos los nodos finales que encontro por medio de id
+                    papu=false; //Variable flag
+                    for(Nodo yas:nodes) //Ciclo báscio para sacar los puntos del nodo final
+                    {
+                        if(yas.getId()==idfin) {
+                            x2 = yas.getX();
+                            y2 = yas.getY();
+                        }
+                    }
+                    while(true) { //Ciclo infinito para recorrer hasta el punto de comienzo
+                        for (Arista b : edges) //Empieza a buscar en las aristas a la que corresponde este nodo final en coordenadas
+                        {
+                            if (b.getX2() == x2 && b.getY2() == y2) { //Si encuentra la arista donde esta el nodo final ahora toma las coordenadas del siguiente punto
+                                x1 = b.getX1();
+                                y1 = b.getY1();
+                                for (Nodo c : nodes) //Busca ahora en los nodos los correspondientes con la arista para aplicar el restar
+                                {
+                                    if (!c.getSelected()) { //Reutilizando el atributo de selected a manera de flag, si ya fue seleccionado el nodo no se lo toma en cuenta
+                                        if (c.getX() == x2 && c.getY() == y2) { //Aqui va a guardar ahora el nodo 2 (su id) comparando las coordenadas
+                                            s2 = c.getId();
+                                            s = c.getFeed(); //Para hacer la resta empezamos con lo que sea que esta en el campo de Feed del nodo 2
+                                        } else if (c.getX() == x1 && c.getY() == y1) { //Aqui va a guardar el nodo 1 (su id( comparando las coordenadas
+                                            s1 = c.getId();
+                                            s = s - c.getStart(); //Luego de agarrar el feed del nodo 2 se resta a eso el Start del nodo 1
+                                        }
+                                    }
+                                }
+                                nodes.get(s1).setSelected(true); //Ya trabajamos los 2 nodos entonces los marcamos
+                                nodes.get(s2).setSelected(true);
+                                s = s - matrix[s1][s2]; /*Al final ahora solo nos queda restar lo que sea que esta en la arista Nota:
+                                                        Tambien esto puede escribirse como s-=b.getPeso() pero el valor del peso ya esta guardado en
+                                                        la matriz asi que a mi comodidad lo acomode asi
+                                                         */
+                                if (s < 0)     //Si resultan negativos convierte 0 la wea qlia,
+                                    s = 0;
+                                if (s < b.getHolgura())
+                                    b.setHolgura(s); //Si el feed es menor lo remplaza, para que asi al final siempre queden los
+                                //feeds mas cortos en caso de convergencia
+                            }
+                            x2 = x1;
+                            y2 = y1; //Recorre ahora el punto 2 a punto 1 para pasar al siguiente nodo conectado
+                            for(int i:principios) //Verifica si el nodo siguiente (de vuelta) es un nodo principal
+                            {
+                                if(s1==i)
+                                    papu=true; //De ser principal cambia la variable papu a true lo cual rompe el ciclo infinito
+                            }
+                            if(papu)
+                                    break;
+                        }
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Toast.makeText(MainActivity.this, "Surgió un error", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        public void pintarHolgura() //Ya para pintar las holguras o bueno hacerlas mas anchas papu V:
+        {
+            for(Arista aux:edges)
+            {
+                if(aux.getHolgura()==0)
+                {
+                    //Funciones para pintar o hacer mas algo no se yo frontend
+                    //P.S Ochinchin Daisuki
+                }
+            }
         }
 
         public double angule(float x1, float y1, float x2, float y2){
